bf1808c1098d9814e51f3657c174acd239de5d6a
AIcodexgen <ranjith.d@varundigitalmedia.com>
2026-02-09 20:56:27 +0530

    Update class-public.php

--- public/class-public.php
+++ public/class-public.php
@@ -1,5 +1,43 @@
 <?php
 namespace RealEstatePro;
+
 class Public_Frontend {
-    public function __construct() {}
+    public function __construct() {
+        add_filter('the_content', [$this, 'modify_property_content']);
+        add_action('wp_head', [$this, 'add_structured_data']);
+    }
+    
+    public function modify_property_content($content) {
+        if (is_singular('property')) {
+            // Add custom content before/after
+            $before = '<div class="property-single-wrapper">';
+            $after = '</div>';
+            return $before . $content . $after;
+        }
+        return $content;
+    }
+    
+    public function add_structured_data() {
+        if (!is_singular('property')) return;
+        
+        $property_id = get_the_ID();
+        $schema = [
+            '@context' => 'https://schema.org',
+            '@type' => 'RealEstateListing',
+            'name' => get_the_title(),
+            'description' => get_the_excerpt(),
+            'url' => get_permalink(),
+            'offers' => [
+                '@type' => 'Offer',
+                'price' => get_post_meta($property_id, '_property_price', true),
+                'priceCurrency' => get_option('realestatepro_currency', 'USD')
+            ],
+            'address' => [
+                '@type' => 'PostalAddress',
+                'streetAddress' => get_post_meta($property_id, '_property_address', true)
+            ]
+        ];
+        
+        echo '<script type="application/ld+json">' . wp_json_encode($schema) . '</script>';
+    }
 }
