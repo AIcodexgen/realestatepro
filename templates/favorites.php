8fdf1e2e73c596784aa8c1e8bd171578ca4125ad
AIcodexgen <ranjith.d@varundigitalmedia.com>
2026-02-09 20:59:18 +0530

    Update favorites.php

--- templates/favorites.php
+++ templates/favorites.php
@@ -1,1 +1,10 @@
-
+<?php if (empty($args['properties']) || !$args['properties']->have_posts()) : ?>
+    <p><?php _e('You have no favorite properties.', 'realestatepro'); ?></p>
+<?php else : ?>
+    <div class="favorites-grid properties-grid">
+        <?php while ($args['properties']->have_posts()) : $args['properties']->the_post(); ?>
+            <?php \RealEstatePro\Template_Loader::get_template_part('property-card', null, ['property_id' => get_the_ID()]); ?>
+        <?php endwhile; ?>
+    </div>
+    <?php wp_reset_postdata(); ?>
+<?php endif; ?>
