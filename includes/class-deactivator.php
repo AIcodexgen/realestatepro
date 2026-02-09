6e76afd94e39609be19d17639a5cd26d3934093c
AIcodexgen <ranjith.d@varundigitalmedia.com>
2026-02-09 20:52:48 +0530

    Update class-deactivator.php

--- includes/class-deactivator.php
+++ includes/class-deactivator.php
@@ -1,7 +1,12 @@
 <?php
 namespace RealEstatePro;
+
 class Deactivator {
     public static function deactivate() {
+        // Clear scheduled hooks
+        wp_clear_scheduled_hook('realestatepro_daily_cron');
+        
+        // Flush rewrite rules
         flush_rewrite_rules();
     }
 }
