1d7fc9073c70351db437947ca7eb34ede430b7c3
AIcodexgen <ranjith.d@varundigitalmedia.com>
2026-02-09 20:59:40 +0530

    Update compare.php

--- templates/compare.php
+++ templates/compare.php
@@ -1,1 +1,9 @@
+<div class="compare-container">
+    <div id="compare-content">
+        <p><?php _e('Loading comparison...', 'realestatepro'); ?></p>
+    </div>
+</div>
 
+<script>
+// Compare functionality is handled by class-compare.php via AJAX
+</script>
